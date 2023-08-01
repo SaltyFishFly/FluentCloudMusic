ac97edfe12dccbcfd0767a6cc4406818d2ea1df2
SaltyFishFly <gfishfly@qq.com>
2023-08-02 02:16:42 +0800

    更新目标版本到Windows 11 build 22000

--- Pages/LoginPage.xaml.cs
+++ Pages/LoginPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using FluentNetease.Classes;
 using FluentNetease.Dialogs;
+using System;
+using System.Diagnostics;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -16,24 +18,13 @@ namespace FluentNetease.Pages
         {
             this.InitializeComponent();
         }
-
-        private void AccountInputBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            CheckInputBoxes();
-        }
-
-        private void PasswordInputBox_PasswordChanged(object sender, RoutedEventArgs e)
-        {
-            CheckInputBoxes();
-        }
-
         private async void LoginButton_Click(object sender, RoutedEventArgs e)
         {
             try
             {
                 int Code = await Account.LoginAsync("86", AccountInputBox.Text, PasswordInputBox.Password);
             }
-            catch
+            catch (Exception ex)
             {
                 _ = new LoginFailedDialog().ShowAsync();
             }
@@ -42,25 +33,6 @@ namespace FluentNetease.Pages
         private void HelpButton_Click(object sender, RoutedEventArgs e)
         {
 
-        }
-
-        private void CheckInputBoxes()
-        {
-            bool AccountIsEmpty = AccountInputBox.Text == string.Empty;
-            bool PasswordIsEmpty = PasswordInputBox.Password == string.Empty;
-            if (!AccountIsEmpty && !PasswordIsEmpty)
-            {
-                LoginButton.IsEnabled = true;
-            }
-            else
-            {
-                LoginButton.IsEnabled = false;
-                PasswordInputBox.IsEnabled = !AccountIsEmpty;
-                if (AccountIsEmpty)
-                {
-                    PasswordInputBox.Password = string.Empty;
-                }
-            }
         }
     }
 }
